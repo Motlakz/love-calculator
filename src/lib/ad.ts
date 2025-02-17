69d09261bb13b21a968402725639068b151cebb6
Motlakz <motlalepulasello5@gmail.com>
2025-02-17 21:43:01 +0200

    update: add new images for ads, create native ad component and replace existing popup, added nice animated hero section for better reach.

---
+++ src/lib/ad.ts
@@ -0,0 +1,53 @@
+export const valentineProducts = [
+    {
+        imageUrl: "/public/dates.jpg",
+        title: "His and Hers Funventure Planner",
+        affiliateLink: "https://amzn.to/3X416y6",
+    },
+    {
+        imageUrl: "/public/lovebox.jpg",
+        title: "Love Box",
+        affiliateLink: "https://amzn.to/3X3voB0",
+    },
+    {
+        imageUrl: "/public/aroma.jpg",
+        title: "Aromatherapy Set",
+        affiliateLink: "https://amzn.to/4k2cBQs",
+    },
+];
+
+export const birthdayProducts = [
+    {
+        imageUrl: "/public/squishies.jpg",
+        title: "Birthday Squishies",
+        affiliateLink: "https://amzn.to/41i2dN0",
+    },
+    {
+        imageUrl: "/public/bday.jpg",
+        title: "Birthday Set",
+        affiliateLink: "https://amzn.to/43iTeNl"
+    },
+    {
+        imageUrl: "/public/rose.jpg",
+        title: "Glass Rose",
+        affiliateLink: "https://amzn.to/4i4fY7U",
+    },
+];
+
+export const anniversaryProducts = [
+    {
+        imageUrl: "/public/butterfly.jpg",
+        title: "Forever Gift",
+        affiliateLink: "https://amzn.to/4bbuqZl",
+    },
+    {
+        imageUrl: "/public/anniversary_necklace.jpg",
+        title: "Rose Necklace",
+        affiliateLink: "https://amzn.to/4i1u3Th",
+    },
+    {
+        imageUrl: "/public/anniversary.jpg",
+        title: "His and Hers Mugs",
+        affiliateLink: "https://amzn.to/3X5bFAO",
+    },
+];
